ab9028a4fa3c7774313993e7517e418660d95387
bahati365 <damien.bahati@mail.utoronto.ca>
2021-09-27 19:57:29 -0400

    I created a class in Tradable.java

--- src/main/java/Tradable.java
+++ src/main/java/Tradable.java
@@ -1,3 +1,14 @@
 public interface Tradable {
     int getPrice();
 }
+    /*This class is upposed to take a an Array of a list as its parameter, so thats one wrong am gonna fix
+    */
+
+piublic class Tradable (String Food, String Shoes {
+    this.Food = Tradable;
+    this.Shoes = Tradable;
+
+}public Tradable{
+    this.Trader = Food;
+    this.Trader = Shoes;
+}
